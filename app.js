b61f796b3df8ae0a8e71c3489f4caa4c83682914
tim <timothy.potts@colorado.edu>
2019-11-24 17:14:24 -0700

    forgot what I added but heres some more

--- app.js
+++ app.js
@@ -5,3 +5,47 @@ const engineer = require('Engineer.js');
 const intern = require('Intern.js');
 const manager = require('Manager.js');
 
+var teamlist = [];
+
+function 
+
+
+function setManager() {
+    console.log('Your team needs a manager.\n');
+    inquirer.prompt([
+        {
+            message: "What is the manager's name?",
+            name: 'managerName',
+            type: 'input'
+        },
+        {
+            message: 'What is their ID number?',
+            name: 'idNumber',
+            type: 'input'
+        },
+        {
+            message: "What is the manager's email?",
+            name: 'manEmail',
+            type: 'input'
+        },
+        {
+            message: 'What is their office number?',
+            name: 'offnum',
+            type: 'input'
+        }
+    ])
+    .then(function(data) {
+        var manager = new manager(data.managerName, data.idNumber, data.manEmail, data.offnum);
+        teamlist.push(manager);
+
+    });
+
+}
+
+
+
+
+function startBuilding() {
+    console.log("Let's build a team");
+    setManager();
+}
